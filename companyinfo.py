4b2f8aba60f461600167b7aae5c989182f3241bf
sachin7893 <54388116+sachin7893@users.noreply.github.com>
2019-08-22 04:14:41 -0500

    Update companyinfo.py

--- companyinfo.py
+++ companyinfo.py
@@ -5,10 +5,10 @@ import requests
 import sys
 import configparser
 
-
+macaddr=str(raw_input("Please enter MAC Address: "))
 config = configparser.ConfigParser()
 config.read("config.txt")
 API_KEY = config.get('client', 'api_key')
-response = requests.get('https://api.macaddress.io/v1?apiKey='+API_KEY+'&output=json&search='+sys.argv[1])
+response = requests.get('https://api.macaddress.io/v1?apiKey='+API_KEY+'&output=json&search='+macaddr)
 data = response.json()
 print (data['vendorDetails']['companyName'])
