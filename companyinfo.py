fb662c2a1734339d4d1f0ac4aae9ca77eb6624cb
sachin7893 <54388116+sachin7893@users.noreply.github.com>
2019-08-22 11:45:18 +0530

    Update companyinfo.py

--- companyinfo.py
+++ companyinfo.py
@@ -11,4 +11,4 @@ config.read("config.txt")
 API_KEY = config.get('client', 'api_key')
 response = requests.get('https://api.macaddress.io/v1?apiKey='+API_KEY+'&output=json&search='+sys.argv[1])
 data = response.json()
-print data['vendorDetails']['companyName']
+print (data['vendorDetails']['companyName'])
